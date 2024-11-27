10fa516a0951d0fe3a751215ed05a51f669b96b5
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2024-11-27 12:29:10 +0000

    added proxy for testing

--- backend/chartwise/chartwise/settings.py
+++ backend/chartwise/chartwise/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'chartwise.urls'
@@ -160,6 +160,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://andreineagoe23.github.io",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
